95d8dca1d7f03221bb0f0a5da03d7f1342440e03
AwrenNuit <a.rennaker@yahoo.com>
2019-11-25 15:23:25 -0600

    made changes for heroku

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,14 +1,34 @@
 const pg = require(`pg`);
+const url = require(`url`);
 
-const config = {
-    database: `to_do`,
-    host: `localhost`,
-    port: 5432,
+let config = {};
+
+if (process.env.DATABASE_URL) {
+
+  const params = url.parse(process.env.DATABASE_URL);
+  const auth = params.auth.split(':');
+  
+  config = {
+    user: auth[0],
+    password: auth[1],
+    host: params.hostname,
+    port: params.port,
+    database: params.pathname.split('/')[1],
+    ssl: true,
     max: 10,
     idleTimeoutMillis: 30000
-};
+  };
+} else {
+  config = {
+    host: 'localhost',
+    port: 5432,
+    database: 'prime_app',
+    max: 10,
+    idleTimeoutMillis: 30000
+  };
+}
 
-const pool = new pg.Pool(config);
+const pool = pg.Pool(config);
 
 pool.on(`connect`, ()=>{
     console.log('connected to PG');
@@ -16,6 +36,7 @@ pool.on(`connect`, ()=>{
 
 pool.on(`error`, (error)=>{
     console.log('error connecting to PG', error);
+    process.exit(-1);
 });
 
 module.exports = pool;
