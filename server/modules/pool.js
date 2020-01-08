09d7a67178ab635a59703f0eb68c4dc46a7e9a82
AwrenNuit <a.rennaker@yahoo.com>
2020-01-07 20:38:21 -0600

    cleaned up routes, added styling

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,42 +1,32 @@
-const pg = require(`pg`);
-const url = require(`url`);
-
+const pg = require('pg');
+const url = require('url');
 let config = {};
 
 if (process.env.DATABASE_URL) {
+    let params = url.parse(process.env.DATABASE_URL);
+    let auth = params.auth.split(':');
 
-  const params = url.parse(process.env.DATABASE_URL);
-  const auth = params.auth.split(':');
-  
-  config = {
-    user: auth[0],
-    password: auth[1],
-    host: params.hostname,
-    port: params.port,
-    database: params.pathname.split('/')[1],
-    ssl: true,
-    max: 10,
-    idleTimeoutMillis: 30000
-  };
+    config = {
+        user: auth[0],
+        password: auth[1],
+        host: params.hostname,
+        port: params.port,
+        database: params.pathname.split('/')[1],
+        ssl: true,
+        max: 10,
+        idleTimeoutMillis: 30000,
+    };
+
 } else {
-  config = {
-    host: 'localhost',
-    port: 5432,
-    database: 'to_do',
-    max: 10,
-    idleTimeoutMillis: 30000
-  };
+    config = {
+        user: process.env.PG_USER || null,
+        password: process.env.DATABASE_SECRET || null,
+        host: process.env.DATABASE_SERVER || 'localhost',
+        port: process.env.DATABASE_PORT || 5432,
+        database: process.env.DATABASE_NAME || 'to_do',
+        max: 10,
+        idleTimeoutMillis: 30000,
+    };
 }
 
-const pool = pg.Pool(config);
-
-pool.on(`connect`, ()=>{
-    console.log('connected to PG');
-});
-
-pool.on(`error`, (error)=>{
-    console.log('error connecting to PG', error);
-    process.exit(-1);
-});
-
-module.exports = pool;
+module.exports = new pg.Pool(config);
