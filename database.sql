6faaaa9abe7094b442b7e2069abb7d91a2a46602
Awren Nuit <54560441+AwrenNuit@users.noreply.github.com>
2020-01-07 23:14:00 -0600

    updated database

--- database.sql
+++ database.sql
@@ -1,7 +1,13 @@
+DROP TABLE "to-do";
+
 CREATE TABLE "to-do" (
 	"id" serial primary key,
-	"task" varchar(80) not null,
-	"completed" varchar(30) DEFAULT 'N'
+	"task" varchar(180) not null,
+	"completed" boolean DEFAULT false
 );
 
 INSERT INTO "to-do"("task") VALUES('Vacuum bedroom');
+INSERT INTO "to-do"("task") VALUES('Clean lounge');
+INSERT INTO "to-do"("task") VALUES('Order kitchen');
+INSERT INTO "to-do"("task") VALUES('Ditch dining room');
+INSERT INTO "to-do"("task") VALUES('Fuel bathroom');
